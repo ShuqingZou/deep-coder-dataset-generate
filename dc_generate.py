c0095b5a6c9c3429a3f2bef50186a43fa84721bf
43c <127343c@gmail.com>
2025-11-27 04:05:22 +0000

    created easier to config dataset generation driver

--- dc_generate.py
+++ dc_generate.py
@@ -11,8 +11,10 @@ from src.generate_dataset import generate_dataset, DatasetSpec, EquivalenceCheck
 from src.program_simplifier import remove_redundant_variables, remove_redundant_expressions, remove_dependency_between_variables
 
 from pathlib import Path
+from typing import List, Tuple, Union, Dict, Callable, Iterator, Iterable
 
 class DCGenerator:
+    @staticmethod
     def _from_cli_args(args: argparse.Namespace):
         return DCGenerator(
             args.num_programs,
@@ -25,11 +27,12 @@ class DCGenerator:
             args.num_prune,
             args.list_max,
             args.pickle_name,
-            args.seed
+            args.seed,
+            args.enum
         )
     
     def __init__(self, num_programs, num_valid, dataset_dir, min_length, max_length,
-                 value_range, num_examples, num_prune, list_max, pickle_name, seed):
+                 value_range, num_examples, num_prune, list_max, pickle_name, seed, enumerate):
         # just storing attribs for fun
         self.seed = seed
         self.num_programs = num_programs
@@ -42,6 +45,7 @@ class DCGenerator:
         self.num_prune = num_prune
         self.list_max = list_max
         self.pickle_name = pickle_name + ".pickle"
+        self.enumerate = enumerate
 
         # rng setup
         self.SEED_MAX = 2**32 - 1
@@ -52,17 +56,22 @@ class DCGenerator:
         random.seed(self.root_rng.randint(self.SEED_MAX))
         np.random.seed(self.root_rng.randint(self.SEED_MAX))
 
+        # get language object (list of generate_io_samples.Function)
         LINQ, _ = generate_io_samples.get_language(value_range)
+
+        # remove the MAP IDT case as it does not improve expressiveness
         self.LINQ = [f for f in LINQ if not "IDT" in f.src]
 
+        # extract minimum and maximum function used in simplification
         self.MINIMUM = to_function([f for f in LINQ if f.src == "MINIMUM"][0])
         self.MAXIMUM = to_function([f for f in LINQ if f.src == "MAXIMUM"][0])
 
-        # setup gen stuff
-        program_iterator = lambda iterator: tqdm(iterator, desc="Program Generation")
+        # setup iteration tracking stuff
+        program_iterator = lambda iterator: tqdm(iterator, desc="Total programs generated", unit="program", bar_format='{l_bar}{bar:40}{r_bar}')
         entry_iterator = lambda iterator: tqdm(iterator, desc="Prune Entries")
         self.iterator_decorator = IteratorDecorator(program_iterator, entry_iterator)
 
+        # setup dataset and pruning heuristic
         self.dataset_spec = DatasetSpec(value_range, list_max, num_examples, min_length, max_length)
         self.eq_spec = EquivalenceCheckingSpec(0, num_prune, np.random.RandomState(seed))
 
@@ -77,7 +86,8 @@ class DCGenerator:
             "num_examples",
             "num_prune",
             "list_max",
-            "seed"
+            "seed",
+            "enumerate"
         ]
 
         s = '\n'.join([f"{k} = {v}" for k, v in vars(self).items() if k in printables])
@@ -97,7 +107,16 @@ class DCGenerator:
                     self.dataset_spec,
                     self.eq_spec,
                     str(out_file), self.num_programs if self.num_programs > 0 else None,
-                    simplify=self.simplify, decorator=self.iterator_decorator)
+                    simplify=self.simplify,
+                    decorator=self.iterator_decorator,
+                    enumerate=self.enumerate)
+
+def _debug_dump_dcg(dcg):
+    print("======DEBUG=======")
+    print("Created DeepCoder dataset generator.")
+    print(repr(dcg))
+    print("==================")
+    print()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -105,7 +124,7 @@ if __name__ == '__main__':
     )
 
     parser.add_argument("-n", "--num-programs", type=int, default=1000000, help = "Number of programs to generate.")
-    parser.add_argument("--num-valid", type=int, default=10, help="Number of valid programs.")
+    parser.add_argument("--num-valid", type=int, default=10, help="Number of validation programs.")
     parser.add_argument("--dir", type=str, default="dataset/", help="Directory to store output datasets in.")
     parser.add_argument("--min-length", type=int, default=1, help="Minimum program length.")
     parser.add_argument("--max-length", type=int, default=5, help="Maximum program length.")
@@ -115,13 +134,12 @@ if __name__ == '__main__':
     parser.add_argument("--list-max", type=int, default=20, help="The maximum length of lists used in the dataset.")
     parser.add_argument("--pickle-name", type=str, default="dataset")
     parser.add_argument("--seed", type=int, default=1234, help="RNG seed.")
-    
+    parser.add_argument('--enum', action='store_true')
+
     args = parser.parse_args()
 
-    dcg = DCGenerator._from_cli_args(args)
-
-    print("Created DeepCoder dataset generator.")
-    print(repr(dcg))  #debuggerino
+    dcg: DCGenerator = DCGenerator._from_cli_args(args)
+    _debug_dump_dcg(dcg)
 
     dcg.generate()
 
